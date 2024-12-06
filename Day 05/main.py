c2e8627fd4db066ffd851c2836147bac26bff84f
Žiga Pregelj <ziga.pregelj@razum.si>
2024-12-06 22:34:42 +0100

    feat: day 5 part two done

--- Day 05/main.py
+++ Day 05/main.py
@@ -1,5 +1,5 @@
 import unittest
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 s = "small_input.txt"
 l = "input.txt"
@@ -25,22 +25,39 @@ small_input: tuple[list[str], list[str]] = read_lines(s)
 large_input: tuple[list[str], list[str]] = read_lines(l)
 
 
-# make dict for after one number
-# make another dict to look up before number
-def make_dict_to_forward_and_backward_lookup(file_path) -> tuple:
+# for dictionary with keys of numbers and values set of numbers that must be printed after that one
+def make_dict_to_forward_lookup(file_path) -> tuple:
     forward_dict = defaultdict(set)
-    backward_dict = defaultdict(set)
     page_ordering_rules, pages_to_produce = read_lines(file_path)
     for rule in page_ordering_rules:
         first, second = rule.split("|")
         forward_dict[int(first)].add(int(second))
-        backward_dict[int(second)].add(int(first))
-    return forward_dict, backward_dict
+    return forward_dict
+
+
+# for part 2 reorder invalid pages to meet requirements and become valid
+def sort_to_valid_pages(file_path, set_of_pages):
+    forward_dict = make_dict_to_forward_lookup(file_path)
+    how_many_element_in_forward_dicts = defaultdict(int)
+    for element_1 in set_of_pages:
+        for element_2 in set_of_pages:
+            # in which element all elements in forward dict
+            # next valid number in an order list must be present in more list/sets of numbers in a forward lookup list
+            if element_1 in forward_dict[element_2]:
+                how_many_element_in_forward_dicts[element_1] += 1
+
+    for element in set_of_pages:
+        if element not in how_many_element_in_forward_dicts:
+            how_many_element_in_forward_dicts[element] = 0
+
+    reverse_how_many_element_in_forward_dicts = {value: key for key, value in how_many_element_in_forward_dicts.items()}
+    return [key for value, key in sorted(reverse_how_many_element_in_forward_dicts.items())]
 
 
 def loop_through_pages(file_path):
     sum_valid_middle_elements = 0
-    forward_dict, backward_dict = make_dict_to_forward_and_backward_lookup(file_path)
+    new_valid_counter = 0
+    forward_dict = make_dict_to_forward_lookup(file_path)
     page_ordering_rules, pages_to_produce = read_lines(file_path)
     for set_of_pages in pages_to_produce:
         set_of_pages = set_of_pages.split(",")
@@ -50,18 +67,20 @@ def loop_through_pages(file_path):
         for i, page in enumerate(set_of_pages):
             current_element_in_page = set_of_pages[i]
             looking_forward_elements = set(set_of_pages[i + 1:])
-            looking_backward_elements = set(set_of_pages[:i])
-            if not looking_forward_elements.issubset(
-                    forward_dict[current_element_in_page]) and looking_backward_elements.issubset(
-                backward_dict[current_element_in_page]):
+            if not looking_forward_elements.issubset(forward_dict[current_element_in_page]):
                 all_valid = False
         if all_valid:
             the_element = set_of_pages[len(set_of_pages) // 2]
             sum_valid_middle_elements += the_element
-    return sum_valid_middle_elements
+        else:
+            new_pages = sort_to_valid_pages(file_path, set_of_pages)
+            the_element = new_pages[len(new_pages) // 2]
+            new_valid_counter += the_element
+    return sum_valid_middle_elements, new_valid_counter
 
 
-print("First part: ", loop_through_pages(l))
+print("First part: ", loop_through_pages(l)[0])
+print("Second part: ", loop_through_pages(l)[1])
 
 
 class TestFunctions(unittest.TestCase):
@@ -69,4 +88,8 @@ class TestFunctions(unittest.TestCase):
         self.small_input = read_lines(s)
 
     def test_loop_through_pages(self):
-        self.assertEqual(loop_through_pages(s), 143)
+        self.assertEqual(loop_through_pages(s)[0], 143)
+        self.assertEqual(loop_through_pages(s)[1], 123)
+
+    def test_sort_to_valid_pages(self):
+        self.assertEqual(sort_to_valid_pages(s, [97, 13, 75, 29, 47]), [97, 75, 47, 29, 13])
