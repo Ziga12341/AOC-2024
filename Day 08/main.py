ddbaca901d9267c1922efc9664777b18708f5cdb
Žiga Pregelj <ziga.pregelj@razum.si>
2024-12-14 08:08:17 +0100

    fix: day 8 part 1: fix unhashable set

--- Day 08/main.py
+++ Day 08/main.py
@@ -16,7 +16,7 @@ large_input: list[str] = read_lines(l)
 print(read_lines(s))
 
 
-def get_antinodes_location(grid: list[str], x, y, x0, y0):
+def get_antinodes_location(grid: list[str], x, y, x0, y0) -> set:
     collect_antiondes = set()
 
     dx_first, dy_first = x - x0, y - y0
@@ -42,7 +42,7 @@ def get_locations_of_antionodes_for_the_same_frequency(grid, set_of_antennas):
         for antena_2 in set_of_antennas:
             x0, y0 = antena_2
 
-            set_all_antinodes_of_frequency += get_antinodes_location(grid, x, y, x0, y0)
+            set_all_antinodes_of_frequency = set_all_antinodes_of_frequency.union(get_antinodes_location(grid, x, y, x0, y0))
     return set_all_antinodes_of_frequency
 
 
@@ -57,7 +57,7 @@ def get_all_frequencies_antennas_location(grid):
 def count_antinodes(grid):
     all_antinodes = set()
     for char, set_of_antenna_location in get_all_frequencies_antennas_location(grid).items():
-        all_antinodes.add(get_locations_of_antionodes_for_the_same_frequency(grid, set_of_antenna_location))
+        all_antinodes = all_antinodes.union(get_locations_of_antionodes_for_the_same_frequency(grid, set_of_antenna_location))
     return len(all_antinodes)
 
 print("First part: ", count_antinodes(small_input))
