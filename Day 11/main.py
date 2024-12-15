6bd286a698309b80192299d1f9e92feabc3ade2b
Žiga Pregelj <ziga.pregelj@razum.si>
2024-12-15 11:27:23 +0100

    feat: day 11 part 2, I do not know how to have smaller lists

--- Day 11/main.py
+++ Day 11/main.py
@@ -1,4 +1,5 @@
 import unittest
+from functools import lru_cache
 
 s = "small_input.txt"
 l = "input.txt"
@@ -11,6 +12,32 @@ def read_lines(file_path: str) -> list[int]:
 
 small_input: list[int] = read_lines(s)
 large_input: list[int] = read_lines(l)
+
+
+# 125
+@lru_cache(maxsize=None)
+def blink_stone(stone):
+    list_after_blink = []
+    if stone == 0:
+        list_after_blink.append(1)
+    else:
+        len_stone = len(str(stone))
+        if len_stone % 2 == 0 and stone != 0:
+            first = str(stone)[:len_stone // 2]
+            second = str(stone)[len_stone // 2:]
+            first_int = int(first)
+            second_int = int(second)
+            if set(first) == {0}:
+                list_after_blink.append(0)
+            else:
+                list_after_blink.append(first_int)
+            if set(second) == {0}:
+                list_after_blink.append(0)
+            else:
+                list_after_blink.append(second_int)
+        else:
+            list_after_blink.append(stone * 2024)
+    return list_after_blink
 
 
 # 512072, 1, 20, 24, 28676032
@@ -40,9 +67,17 @@ def blink(list_of_arrangement):
     return list_after_blink
 
 
-def next_blink(file_path):
-    arrangement = read_lines(file_path)
-    return blink(arrangement)
+print(blink([125]))
+
+
+# i may need to remember on which step i (result of blink) split something and and save only small list... than on the last tstep iterarte only
+
+def next_blink(list_of_stones):
+    next_blink_list = []
+    arrangement = [blink_stone(stone) for stone in list_of_stones]
+    for new_stone in arrangement:
+        next_blink_list.extend(new_stone)
+    return next_blink_list
 
 
 def blink_n_times(first_arrangement, n):
@@ -50,7 +85,34 @@ def blink_n_times(first_arrangement, n):
     if n == 0:
         return next_arrangement
     else:
-        next_arrangement = (blink_n_times(blink(next_arrangement), n - 1))
+        next_arrangement = (blink_n_times(next_blink(next_arrangement), n - 1))
+        return next_arrangement
+
+
+@lru_cache(maxsize=None)
+def blink_stone_n_times(first_number, n):
+    next_arrangement = [first_number]
+    for i in range(len(next_arrangement)):
+        first_blink = blink_stone(next_arrangement[i])
+        if n == 0:
+            return next_arrangement
+        else:
+            for blink in first_blink:
+                next_arrangement.append(blink_stone_n_times(blink, n - 1))
+            return next_arrangement
+
+
+# ------------ #
+
+def blink_first_stone_n_times(stone, n):
+    next_arrangement = stone
+    if n == 0:
+        return next_arrangement
+    else:
+        current_arrangement = []
+        for stone in next_arrangement:
+            current_arrangement.extend(blink_stone_n_times(blink_stone(stone), n - 1))
+        next_arrangement = current_arrangement
         return next_arrangement
 
 
@@ -58,7 +120,20 @@ def count_stones(first_arrangement, n):
     return len(blink_n_times(first_arrangement, n))
 
 
+def count_stones_second_part(first_arrangement, n):
+    return len(blink_stone_n_times(first_arrangement, n))
+
+
+# print("blink", blink_stone_n_times(17, 6))
+
+
+# print("blink first stone", blink_first_stone_n_times([125], 5))
+
 print("First part: ", count_stones(large_input, 25))
+
+
+# print("Second part: ", count_stones_second_part(large_input, 25))
+# print("Second part: ", count_stones_second_part(large_input, 40))
 
 
 class TestFunctions(unittest.TestCase):
@@ -72,7 +147,7 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(blink([512072, 1, 20, 24, 28676032]), [512, 72, 2024, 2, 0, 2, 4, 2867, 6032])
 
     def test_count_stones(self):
-        self.assertEqual(next_blink(self.s), [253000, 1, 7])
+        self.assertEqual(next_blink(read_lines(self.s)), [253000, 1, 7])
 
     def test_blink_n_times(self):
         self.assertEqual(count_stones(self.small_input, 6), 22)
