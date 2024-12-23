8227a8c496f3847e98a6027df4c73b8240e2bd20
Žiga Pregelj <ziga.pregelj@razum.si>
2024-12-23 13:54:24 +0100

    feat: day 23 part 1 done

--- Day 23/main.py
+++ Day 23/main.py
@@ -11,9 +11,6 @@ def list_of_connected_computers(file: str) -> list:
         return [line.strip().split("-") for line in file]
 
 
-print(list_of_connected_computers(s))
-
-
 def create_connected_dicts(file: str) -> tuple[defaultdict[Any, list], defaultdict[Any, list]]:
     with open(file, "r", encoding="utf-8") as file:
         connected_computers = defaultdict(list)
@@ -27,20 +24,14 @@ def create_connected_dicts(file: str) -> tuple[defaultdict[Any, list], defaultdi
         return connected_computers, reversed_connected_computers
 
 
-small_input: tuple[defaultdict[Any, list], defaultdict[Any, list]] = create_connected_dicts(s)
-
-
-# large_input: tuple[defaultdict[Any, list], defaultdict[Any, list]] = create_connected_dicts(l)
-
-
 def sets_of_networks(file_name):
     networks_by_3_comp = set()
+    all_in_network = set()
     connected_computers, reversed_connected_computers = create_connected_dicts(file_name)
     for first, second in list_of_connected_computers(file_name):
         third_computer_in_network = (set(connected_computers[first] + reversed_connected_computers[first]) & set(
-            connected_computers[second] + reversed_connected_computers[second])) | (set(
-            connected_computers[second] + reversed_connected_computers[second]) & set(
-            connected_computers[first] + reversed_connected_computers[first]))
+            connected_computers[second] + reversed_connected_computers[second]))
+
         if third_computer_in_network:
             for i in range(len(third_computer_in_network)):
                 third = third_computer_in_network.pop()
@@ -52,9 +43,22 @@ def sets_of_networks(file_name):
 print(sets_of_networks(s))
 
 
+def one_computer_start_with_t(file_name):
+    counter = 0
+    for network in sets_of_networks(file_name):
+        first, second, third = network
+        if first[0] == 't' or second[0] == 't' or third[0] == 't':
+            counter += 1
+    return counter
+
+
+print("Part 1:", one_computer_start_with_t(l))
+
+
 class TestFunctions(unittest.TestCase):
     def setUp(self):
         self.s = "small_input.txt"
+        self.l = "input.txt"
 
     def test_creating_networks(self):
         self.assertEqual(len(sets_of_networks(self.s)), 12)
@@ -63,3 +67,7 @@ class TestFunctions(unittest.TestCase):
                           ('aq', 'cg', 'yn'), ('tc', 'td', 'wh'), ('co', 'ka', 'ta'), ('co', 'de', 'ka'),
                           ('qp', 'td', 'wh'), ('de', 'ka', 'ta'), ('co', 'de', 'ta'), ('ub', 'vc', 'wq')}
                          )
+
+    def test_count_t(self):
+        self.assertEqual(one_computer_start_with_t(self.s), 7)
+        self.assertEqual(one_computer_start_with_t(self.l), 1368)
