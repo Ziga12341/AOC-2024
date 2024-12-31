1b3bcee85da5ad280da2691681564dab409747ad
Žiga Pregelj <ziga.pregelj@razum.si>
2024-12-31 13:47:08 +0100

    feat: day 13 part 2 first test passed with new approach

--- Day 13/main.py
+++ Day 13/main.py
@@ -40,39 +40,46 @@ def calculate_lowest_price(xa, ya, xb, yb, xp, yp):
                 collect_prices.append(press_a * 3 + press_b)
     return min(collect_prices) if collect_prices else 0
 
+
 def calculate_lowest_price_part_2(xa, ya, xb, yb, xp, yp):
     # calculation for x
-    xp = xp + 10000000000000
-    yp = yp + 10000000000000
+    # xp = xp + 10000000000000
+    # yp = yp + 10000000000000
     get_all_price_x_times_press_a = set()
-    for n in range(xp // xa):
+    for n in reversed(range(xp // xa)):
         if (xp - n * xa) % xb == 0:
-            get_all_price_x_times_press_a.add(n)
-
-    # calculation for y
-    get_all_price_y_times_press_a = set()
-    for m in range(yp // ya):
-        if (yp - m * ya) % yb == 0:
-            get_all_price_y_times_press_a.add(m)
-
-    # biggest element that is in for both x and y price times_press_a
-
-    times_press_a_in_x_and_y = get_all_price_y_times_press_a & get_all_price_x_times_press_a
-    # handle classes where we do not have any match
-    if times_press_a_in_x_and_y:
-        biggest_button_a_for_x_and_y = max(times_press_a_in_x_and_y)
-    else:
-        return 0
-    # calculate press b
-    press_b = (xp - biggest_button_a_for_x_and_y * xa) // xb
-
-    return biggest_button_a_for_x_and_y * 3 + press_b
+            if (yp - n * ya) % yb == 0:
+                return (n * 3) + (xp - n * xa) // xb
+            else:
+                return 0
+    #
+    # # calculation for y
+    # get_all_price_y_times_press_a = set()
+    # for m in range(yp // ya):
+    #     if (yp - m * ya) % yb == 0:
+    #         get_all_price_y_times_press_a.add(m)
+    #
+    # # biggest element that is in for both x and y price times_press_a
+    #
+    # times_press_a_in_x_and_y = get_all_price_y_times_press_a & get_all_price_x_times_press_a
+    # # handle classes where we do not have any match
+    # if times_press_a_in_x_and_y:
+    #     biggest_button_a_for_x_and_y = max(times_press_a_in_x_and_y)
+    # else:
+    #     return 0
+    # # calculate press b
+    # press_b = (xp - biggest_button_a_for_x_and_y * xa) // xb
+    #
+    # return biggest_button_a_for_x_and_y * 3 + press_b
 
 
 print(calculate_lowest_price_part_2(94, 34, 22, 67, 8400, 5400))
+
+
 def sum_lowest_price_per_claw_machines(file_name):
     return sum(calculate_lowest_price(button_a[0], button_a[1], button_b[0], button_b[1], price[0], price[1]) for
                button_a, button_b, price in read_lines(file_name))
+
 
 def sum_lowest_price_per_claw_machines_second_part(file_name):
     return sum(calculate_lowest_price_part_2(button_a[0], button_a[1], button_b[0], button_b[1], price[0], price[1]) for
