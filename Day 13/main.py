8ef8ba31857049b8a4bd34a187a395aa6b8153fc
Žiga Pregelj <ziga.pregelj@razum.si>
2024-12-31 07:41:58 +0100

    feat: day 13 part 1, logic for the main function done

--- Day 13/main.py
+++ Day 13/main.py
@@ -12,8 +12,29 @@ def read_lines(file: str) -> list:
 small_input: list[str] = read_lines(s)
 large_input: list[str] = read_lines(l)
 
+print(small_input)
+
+
+def calculate_lowest_price(xa, ya, xb, yb, xp, yp):
+    collect_prices = []
+    for press_a in range(1, 101):
+        for press_b in range(1, 101):
+
+            # sum button a and b (times each button pressed) and compare with price
+            if (xp, yp) == ((press_a * xa + press_b * xb), (press_a * ya + press_b * yb)):
+                # press a cost 3 tokens, sum presses
+                collect_prices.append(press_a * 3 + press_b)
+    return min(collect_prices) if collect_prices else 0
+
+
+print(calculate_lowest_price(94, 34, 22, 67, 8400, 5400))
+
 
 class TestFunctions(unittest.TestCase):
     def setUp(self):
         self.small_input: list[str] = read_lines(s)
 
+    def test_lowest_price(self):
+        self.assertEqual(calculate_lowest_price(94, 34, 22, 67, 8400, 5400), 280)
+        self.assertEqual(calculate_lowest_price(17, 86, 84, 37, 7870, 6450), 200)
+        self.assertEqual(calculate_lowest_price(7, 8, 8, 7, 7870, 6450), 0)
